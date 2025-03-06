a99113df195c05d760c6dc1321d0dbde08e32daa
Geoff Norris <gn@bellaliant.net>
2025-03-06 10:32:25 -0400

    reverted from postgresql to mysql, added relationships and reimplemented foreign keys using foreign keys

--- moviedb.sql
+++ moviedb.sql
@@ -1,26 +1,26 @@
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = "moviedb") THEN
-        CREATE DATABASE moviedb;
-    END IF;
-END $$;
-\c moviedb
+
+CREATE DATABASE moviedb IF NOT EXISTS;
+
+USE moviedb;
 
 CREATE TABLE IF NOT EXISTS account (
     username VARCHAR(50) PRIMARY KEY, 
-    password VARCHAR(50) NOT NULL, --need to figure out hashing
-    joinDate Date NOT NULL
+    password VARCHAR(50) NOT NULL, --figure out hashing
+    joinDate DATE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS admin (
+    username VARCHAR(50) PRIMARY KEY, 
+    FOREIGN KEY (username) REFERENCES account(username)
+);
 
-) INHERITS (account);
-
-CREATE TABLE IF NOT EXISTS user ( 
+CREATE TABLE IF NOT EXISTS user (
+    username VARCHAR(50) PRIMARY KEY, 
     -- friendCount INT, derived attribute
     -- reviewCount INT, derived attribute
 --postgresql allows for automatically computing values for derived attributes, maybe try that later
-) INHERITS (account);
+    FOREIGN KEY (username) REFERENCES account(username)
+);
 
 CREATE TABLE IF NOT EXISTS bookmark (
     ratingID SERIAL PRIMARY KEY,
@@ -34,20 +34,53 @@ CREATE TABLE IF NOT EXISTS media (
     mediaID INT PRIMARY KEY,
     name VARCHAR(50),
     description VARCHAR(300), -- need to check max length of a description on tmdb
-    --aggregateRating FLOAT, this is derived
     MPARating VARCHAR(5)
 );
 
 CREATE TABLE IF NOT EXISTS movie (
+    mediaID INT PRIMARY KEY,
     length int, -- not sure if this should be an int
-) INHERITS (media);
+    FOREIGN KEY (mediaID) REFERENCES media(mediaID)
+);
 
 CREATE TABLE IF NOT EXISTS TVShow (
-    episodeCount int
-) INHERITS (media);
+    mediaID INT PRIMARY KEY,
+    episodeCount int,
+    FOREIGN KEY (mediaID) REFERENCES media(mediaID)
+);
 
 CREATE TABLE IF NOT EXISTS castCrew (
     actorID INT PRIMARY KEY,
     name VARCHAR(50),
     biography VARCHAR(300) -- once again not sure on the length, check tmdb
 );
+
+-- relationships being defined here
+CREATE TABLE IF NOT EXISTS CONTRIBUTED (
+    mediaID INT,
+    actorID INT,
+    role VARCHAR(50),
+    FOREIGN KEY (mediaID) REFERENCES media(mediaID),
+    FOREIGN KEY (actorID) REFERENCES castCrew(actorID)
+);
+
+CREATE TABLE IF NOT EXISTS CREATES (
+    username VARCHAR(50),
+    ratingID INT,
+    FOREIGN KEY (username) REFERENCES user(username),
+    FOREIGN KEY (ratingID) REFERENCES bookmark(ratingID)
+);
+
+CREATE TABLE IF NOT EXISTS ABOUT (
+    ratingID INT,
+    mediaID INT,
+    FOREIGN KEY (ratingID) REFERENCES bookmark(ratingID),
+    FOREIGN KEY (mediaID) REFERENCES media(mediaID)
+);
+
+CREATE TABLE IF NOT EXISTS FRIENDS_WITH (
+    username1 VARCHAR(50),
+    username2 VARCHAR(50), 
+    FOREIGN KEY (username1) REFERENCES user(username),
+    FOREIGN KEY (username2) REFERENCES user(username)
+)
