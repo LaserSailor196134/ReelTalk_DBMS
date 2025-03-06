39230568f62436f54da2a3bfe5f4ee7d9fe94106
Geoffrey Norris <0302433n@acadiau.ca>
2025-03-06 00:03:47 -0400

    fixed multiple bugs, including improper inheritance, as well as improper IF NOT EXISTS. Switched to postgresql from sql for now to streamline inheritance. Replaced improper brackets with correct ones

--- moviedb.sql
+++ moviedb.sql
@@ -1,48 +1,48 @@
-IF NOT EXISTS CREATE DATABASE moviedb;
+CREATE DATABASE IF NOT EXISTS moviedb;
 
-USE moviedb;
+\c moviedb
 
-IF NOT EXISTS CREATE TABLE account {
+CREATE TABLE IF NOT EXISTS account (
     username VARCHAR(50) PRIMARY KEY, 
     password VARCHAR(50) NOT NULL, --need to figure out hashing
     joinDate Date NOT NULL
-};
+);
 
-IF NOT EXISTS CREATE TABLE admin : account {
+CREATE TABLE IF NOT EXISTS admin (
 
-};
+) INHERITS (account);
 
-IF NOT EXISTS CREATE TABLE user : account {
+CREATE TABLE IF NOT EXISTS user : account (
     friendCount INT NOT NULL,
     reviewCount INT NOT NULL
-};
+) INHERITS (account);
 
-IF NOT EXISTS CREATE TABLE bookmark {
+CREATE TABLE IF NOT EXISTS bookmark (
     ratingID INT PRIMARY KEY,
     watchStatus VARCHAR(10) NOT NULL,
     numberRating INT,
     description VARCHAR(300), -- not sure of the length, should manage max length on front end as well
     dateCreated DATE NOT NULL
-};
+);
 
-IF NOT EXISTS CREATE TABLE media {
+CREATE TABLE IF NOT EXISTS media (
     mediaID INT PRIMARY KEY,
     name VARCHAR(50),
     description VARCHAR(300), -- need to check max length of a description on tmdb
     aggregateRating FLOAT,
     MPARating VARCHAR(5)
-};
+);
 
-IF NOT EXISTS CREATE TABLE movie : media {
+CREATE TABLE IF NOT EXISTS movie : media (
     length int, -- not sure if this should be an int
-};
+);
 
-IF NOT EXISTS CREATE TABLE TVShow : media {
+CREATE TABLE IF NOT EXISTS TVShow : media (
     episodeCount int
-};
+);
 
-IF NOT EXISTS CREATE TABLE castCrew {
+CREATE TABLE IF NOT EXISTS castCrew (
     actorID INT PRIMARY KEY,
     name VARCHAR(50),
     biography VARCHAR(300) -- once again not sure on the length, check tmdb
-};
+);
