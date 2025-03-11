2aff3362f135dbee6a3fc51247ce76d60d39225c
Geoff Norris <gn@bellaliant.net>
2025-03-11 16:47:49 -0300

    created config file to connect to the database

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+define("HOST", "Localhost");
+define("USER", "movieguy");
+// password should be put in a JSON instead of hardcoded
+define("PASSWORD", "lovemovies"); 
+define("DATABASE", "moviedb");
+
+$movies = new mysqli(HOST, USER, PASSWORD, DATABASE);
+//idiom
+if ($movies === false) {
+    die("ERROR: Could not connect. " . $movies->connect_error);
+} else {
+    echo "Connected successfully";
+}
+?>
