2860215114ef13255a720fc0d3aec3d88be56e8b
Geoff Norris <gn@bellaliant.net>
2025-03-13 10:01:09 -0300

    changed "users" to "dbuser" in register.php

--- register.php
+++ register.php
@@ -4,14 +4,14 @@ include "config.php";
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['uname'];
     $password = $_POST['password'];
-    $checkusername = $movies -> prepare('SELECT * FROM users WHERE username = ?');
+    $checkusername = $movies -> prepare('SELECT * FROM dbuser WHERE username = ?');
     $checkusername -> bind_param('s', $username);
     $checkusername -> execute();
     $checkusername -> store_result();
     if($checkusername -> num_rows > 0) {//if the username already exists
         echo "Username already exists";
     } else {
-        $register = $movies -> prepare ('INSERT INTO users (username, password) VALUES (?, ?)');
+        $register = $movies -> prepare ('INSERT INTO dbuser (username, password) VALUES (?, ?)');
         $register -> bind_params('ss', $username, $password);
         $register -> execute();
         header('Location: login.html');
