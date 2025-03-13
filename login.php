3ec596928a7f0b377c101ad3bdd978a5e9f8a4d0
Geoff Norris <gn@bellaliant.net>
2025-03-13 17:03:59 -0300

    created login.php

---
+++ login.php
@@ -0,0 +1,50 @@
+<?php
+include "config.php";
+
+$username = $POST["uname"];
+$password = $POST["password"];
+$checkusername = $movies -> prepare("SELECT password FROM account WHERE username = ?");
+$checkusername -> bind_param("s", $username);
+$checkusername -> execute();
+$checkusername -> store_result();
+//account not found
+if($checkusername -> num_rows == 0) {
+    echo('<script>
+        alert("Account does not exist");
+        window.location.href = "login.html";
+        </script>');
+    close($checkusername);
+    close($movies);
+    die();
+}
+$checkusername -> bind_result($storedpassword);
+$checkusername -> fetch();
+//if account exists and password is correct
+if(password_verify($password, $storedpassword)) {
+    if(!session_start()) {
+        echo('<script>
+            alert("Session failed to start");
+            window.location.href = "login.html";
+            </script>');
+        close($checkusername);
+        close($movies);
+        die();
+    }
+    $_SESSION["username"] = $username;
+    echo('<script>
+        alert("logged in successfully");
+        window.location.href = "home.html";
+        </script>');
+    close($checkusername);
+    close($movies);
+    die();
+} else {
+    echo('<script>
+        alert("Incorrect password");
+        window.location.assign("login.html");
+        </script>');
+    close($checkusername);
+    close($movies);
+    die();
+}   
+?>
