622e0e15ea3cb3482f7a4dfd00caeb21ed4462c6
Geoff Norris <gn@bellaliant.net>
2025-03-13 15:20:46 -0300

    added alert upon account deletion, added case for if account does not exist

--- delete.php
+++ delete.php
@@ -8,6 +8,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     $checkusername -> bind_param("s", $username);
     $checkusername -> execute();
     $checkusername -> store_result();
+    if($checkusername -> num_rows == 0) {
+        echo('<script>
+            alert("Account does not exist");
+            window.location.href = "delete.html";
+            </script>');
+        
+    }
     if($checkusername -> fetch() == hash("sha256", $password)) {
         $deleteuser = $movies -> prepare("DELETE FROM dbuser WHERE username = ?");
         $deleteuser -> bind_param("s", $username);
@@ -15,7 +22,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
         $deleteaccount = $movies -> prepare("DELETE FROM account WHERE username = ?");
         $deleteaccount -> bind_param("s", $username);
         $deleteaccount -> execute();
-        header("Location: login.html");
+        echo('<script>
+            alert("Account deleted");
+            window.location.href = "login.html";
+            </script>');
         close($checkusername);
         close($deleteuser);
         close($deleteaccount);
