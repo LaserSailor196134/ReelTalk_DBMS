d0354eb0fd78db709fc27dcac8a64c261776c4c0
Geoff Norris <gn@bellaliant.net>
2025-03-13 09:49:33 -0300

    created account registration

---
+++ register.php
@@ -0,0 +1,19 @@
+<?php
+include "config.php";
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $checkusername = $movies -> prepare('SELECT * FROM users WHERE username = ?');
+    $checkusername -> bind_param('s', $username);
+    $checkusername -> execute();
+    $checkusername -> store_result();
+    if($checkusername -> num_rows > 0) {//if the username already exists
+        echo "Username already exists";
+    } else {
+        $register = $movies -> prepare ('INSERT INTO users (username, password) VALUES (?, ?)');
+        $register -> bind_params('ss', $username, $password);
+        $register -> execute();
+    }
+}
+?>
