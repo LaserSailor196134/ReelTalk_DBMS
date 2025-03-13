22721b541564b6a767b0e44c898abd50a66c20cc
Geoffrey Norris <0302433n@acadiau.ca>
2025-03-13 14:57:25 -0300

    added delete account function, untested

---
+++ delete.php
@@ -0,0 +1,32 @@
+<?php
+include "config.php";
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["uname"];
+    $password = $_POST["password"];
+    $checkusername = $movies -> prepare("SELECT password FROM account WHERE username = ?");
+    $checkusername -> bind_param("s", $username);
+    $checkusername -> execute();
+    $checkusername -> store_result();
+    if($checkusername -> fetch() == password) {
+        $deleteuser = $movies -> prepare("DELETE FROM dbuser WHERE username = ?");
+        $deleteuser -> bind_param("s", $username);
+        $deleteuser -> execute();
+        $deleteaccount -> $movies -> prepare("DELETE FROM account WHERE username = ?");
+        $deleteaccount -> bind_param("s", $username);
+        $deleteaccount -> execute();
+        header("Location: login.html");
+        close($checkusername);
+        close($deleteuser);
+        close($deleteaccount);
+        close($movies);
+        die();
+    } else {
+        echo('<script>
+            alert("Incorrect password");
+            window.location.href = "delete.html";');
+        close($checkusername);
+        close($movies);
+    }
+}
+?>
--- register.php
+++ register.php
@@ -6,16 +6,17 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['uname'];
     $password = $_POST['password'];
     //checking if the username already exists
-    $checkusername = $movies -> prepare('SELECT * FROM dbuser WHERE username = ?');
+    $checkusername = $movies -> prepare('SELECT username FROM dbuser WHERE username = ?');
     $checkusername -> bind_param('s', $username);
     $checkusername -> execute();
     $checkusername -> store_result();
-    if($checkusername -> num_rows > 0) {//if the username already exists  
-
+    if($checkusername -> num_rows > 0) {//if the username already exists
         echo('<script>
             alert("username already exists");
             window.location.href = "register.html";
         </script>');
+        close($checkusername);
+        close($movies);
         die();
     } else {
         //register account first
@@ -27,6 +28,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
         $registeruser -> bind_param('s', $username);
         $registeruser -> execute();
         header('Location: login.html');
+        close($checkusername);
         close($registeraccount);
         close($registeruser);
         close($movies);
