92e3df9a17059a1ff18da69d297a0ce99eee8d3b
Geoffrey Norris <0302433n@acadiau.ca>
2025-03-24 13:43:27 -0300

    optimized query for finding all bookmarks for a given profile

--- socialFeatures/profile.php
+++ socialFeatures/profile.php
@@ -2,20 +2,15 @@
 include "../checkloggedin.php";
 if(isLoggedIn()) { //if user is logged in
     include "config.php";
-    $loadProfile = $movies -> prepare('SELECT bookmark.* FROM CREATES
+    $loadProfile = $movies -> prepare('SELECT bookmark.*, media.name FROM CREATES
     JOIN bookmark ON CREATES.ratingID = bookmark.ratingID
+    JOIN ABOUT ON bookmark.ratingID = ABOUT.ratingID
+    JOIN media ON ABOUT.mediaID = media.mediaID
     WHERE CREATES.username = ?'); //get information for every bookmark related to a specified username
     $loadProfile -> bind_param($_SESSION['username']);
     $loadProfile -> execute();
-    $loadProfile -> store_result($ratingID, $watchStatus, $numberRating, $description, $dateCreated);
+    $loadProfile -> store_result($ratingID, $watchStatus, $numberRating, $description, $dateCreated, $movie);
     while(loadProfile -> fetch()) {
-        $findMovie = $movies -> prepare('SELECT media.name FROM ABOUT
-        JOIN media ON ABOUT.mediaID = media.mediaID
-        WHERE ABOUT.ratingID = ?'); //find the name of the movie that is referenced by the bookmark
-        $findMovie -> bind_param($ratingID);
-        $findMovie -> execute();
-        $findMovie -> store_result($movieName);
-        $findMovie -> fetch();
         echo ("<p>watch status: $watchStatus </p>
         <p> movie Name $movie </p>
         <p> rating: $numberRating </p>
