be25c46fac4c8743d4b93de53f823745f6d499be
LaserSailor196134 <gn@bellaliant.net>
2025-03-24 11:08:20 -0300

    made the code work

--- SearchForUsers.php
+++ SearchForUsers.php
@@ -3,8 +3,7 @@
 
     if($_SERVER['REQUEST_METHOD'] == 'POST') {
         $searchterm = $_POST['usersearch'];
-        $searchbutton = $_POST['searchbutton'];
-        echo('hi')
+        echo("$searchterm");
     }
     //echo('SELECT * FROM dbuser WHERE username LIKE $searchterm%') 
 ?>
