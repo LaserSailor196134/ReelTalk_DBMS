c23fed47f7dbaa2ff7324dfe4fbd45a4be81efba
Geoffrey Norris <0302433n@acadiau.ca>
2025-03-24 22:44:45 -0300

    added urlencode whre it was needed and updated todo list

--- Social/SearchForUsers.php
+++ Social/SearchForUsers.php
@@ -11,10 +11,11 @@
     $findUsers -> execute();
     $findUsers -> bind_result($username,$joinDate);
     while($findUsers -> fetch()){ //while get next result returns values
+        $encoded = urlencode($username);
         echo("$username --- Joined: $joinDate");
         echo("
         <form name=\"profile\" method=\"GET\" action=\"profile.php\">
-            <input type=\"hidden\" name=\"username\" value=\"$username\">
+            <input type=\"hidden\" name=\"username\" value=\"$encoded\">
             <input class=\"btn btn-warning p-2 mt-4\" type=\"submit\" value=\"View profile\">
         </form>");
         if($loggedIn && $_SESSION['username'] != $username) {//option to follow them if you are logged in, cannot follow yourself, might want to add a way to prevent having a follow button on people who are already being followed 
