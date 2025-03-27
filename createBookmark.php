c35911ebacb845f47ca15a7ef7981401558b1713
LaserSailor196134 <gn@bellaliant.net>
2025-03-26 21:27:12 -0300

    created framework for checking if a bookmark already exists

--- createBookmark.php
+++ createBookmark.php
@@ -11,24 +11,27 @@
     $review = $_POST['review'];
     $rating = $_POST['rating'];
 
-    $saveBookmark = $movies -> prepare("INSERT INTO bookmark(watchStatus,numberRating,description) VALUES (?,?,?)");
-    $saveBookmark -> bind_param('sis',$watchStatus,$rating,$review);
-    $saveBookmark -> execute();
-    $bmID = $movies -> prepare("SELECT LAST_INSERT_ID()");
-    $bmID -> execute();
-    $bmID -> bind_result($bookmarkID);
-    $bmID -> fetch();
-    $bmID -> close();
+    $isAlreadyReviewed = $movies -> prepare('');//create a query that checks for duplicate reviews on the same movie from the same user
+    
+    if($isAlreadyReviewed -> num_rows() == 0) {//if not already reviewed
+        $saveBookmark = $movies -> prepare("INSERT INTO bookmark(watchStatus,numberRating,description) VALUES (?,?,?)");
+        $saveBookmark -> bind_param('sis',$watchStatus,$rating,$review);
+        $saveBookmark -> execute();
+        $bmID = $movies -> prepare("SELECT LAST_INSERT_ID()");
+        $bmID -> execute();
+        $bmID -> bind_result($bookmarkID);
+        $bmID -> fetch();
+        $bmID -> close();
 
-    $saveBookmarkC = $movies -> prepare("INSERT INTO CREATES(username,ratingID) VALUES (?,?)");
-    $saveBookmarkC -> bind_param('si',$username,$bookmarkID);
-    $saveBookmarkC -> execute();
+        $saveBookmarkC = $movies -> prepare("INSERT INTO CREATES(username,ratingID) VALUES (?,?)");
+        $saveBookmarkC -> bind_param('si',$username,$bookmarkID);
+        $saveBookmarkC -> execute();
 
-    $saveBookmarkA = $movies -> prepare("INSERT INTO ABOUT(ratingID,mediaID) VALUES (?,?)");
-    $saveBookmarkA -> bind_param('ii',$bookmarkID,$mediaID);
-    $saveBookmarkA -> execute();
-
-    echo("Successfully Saved Review")
+        $saveBookmarkA = $movies -> prepare("INSERT INTO ABOUT(ratingID,mediaID) VALUES (?,?)");
+        $saveBookmarkA -> bind_param('ii',$bookmarkID,$mediaID);
+        $saveBookmarkA -> execute();
+    }
+    header("Location: social/profile.php?username = $username");
 
 ?>
 </body>
